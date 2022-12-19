6860d3bb02df95db33b492209908c04926acc2c7
Wasin Wasantivong <chotongza@gmail.com>
2022-12-19 22:03:24 +0700

    finished

--- hw4/lib/main.dart
+++ hw4/lib/main.dart
@@ -67,244 +67,256 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        leading: const IconButton(
-          onPressed: null,
-          icon: Icon(
-            Icons.arrow_back_ios,
-            color: Colors.pinkAccent,
+        appBar: AppBar(
+          leading: const IconButton(
+            onPressed: null,
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.pinkAccent,
+            ),
           ),
+          centerTitle: true,
+          title: Text(widget.title),
+          actions: const [
+            IconButton(onPressed: null, icon: Icon(Icons.ios_share_rounded)),
+            IconButton(
+                onPressed: null, icon: Icon(Icons.favorite_border_outlined))
+          ],
         ),
-        centerTitle: true,
-        title: Text(widget.title),
-        actions: const [
-          IconButton(onPressed: null, icon: Icon(Icons.ios_share_rounded)),
-          IconButton(
-              onPressed: null, icon: Icon(Icons.favorite_border_outlined))
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Align(
-              alignment: Alignment.topCenter,
-              child: CarouselSlider(
-                options: CarouselOptions(height: 200.0),
-                items: images.map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: EdgeInsets.symmetric(horizontal: 5.0),
-                        child:
-                            // 'text $i',
-                            // style: TextStyle(fontSize: 16.0),
-                            Image.asset(
-                          i,
-                          fit: BoxFit.cover,
-                        ),
-                      );
-                    },
-                  );
-                }).toList(),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 10),
+              Align(
+                alignment: Alignment.topCenter,
+                child: CarouselSlider(
+                  options: CarouselOptions(height: 200.0),
+                  items: images.map((i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.symmetric(horizontal: 5.0),
+                          child:
+                              // 'text $i',
+                              // style: TextStyle(fontSize: 16.0),
+                              Image.asset(
+                            i,
+                            fit: BoxFit.cover,
+                          ),
+                        );
+                      },
+                    );
+                  }).toList(),
+                ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(left: 10, top: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text("UNESCO Sustainable Travel Pledge"),
-                  ),
-                  SizedBox(height: 10),
-                  const Text(
-                    'Shangri-La Chiang Mai',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-                  ),
-                  Container(
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Icon(
-                          Icons.star,
-                          color: Colors.grey[500],
-                        ),
-                        Icon(
-                          Icons.star,
-                          color: Colors.grey[500],
-                        ),
-                        Icon(
-                          Icons.star,
-                          color: Colors.grey[500],
-                        ),
-                        Icon(
-                          Icons.star,
-                          color: Colors.grey[500],
-                        ),
-                        Icon(
-                          Icons.star,
-                          color: Colors.grey[500],
-                        ),
-                      ],
+              Container(
+                padding: const EdgeInsets.only(left: 10, top: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text("UNESCO Sustainable Travel Pledge"),
                     ),
-                  ),
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Luxury hotel with free water park, near Chiang Mai Night Bazaar',
-                      style: TextStyle(
-                          fontWeight: FontWeight.normal, fontSize: 20),
+                    SizedBox(height: 10),
+                    const Text(
+                      'Shangri-La Chiang Mai',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(left: 10, top: 50),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text("9.0/10 Superb",
+                    Container(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Icon(
+                            Icons.star,
+                            color: Colors.grey[500],
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.grey[500],
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.grey[500],
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.grey[500],
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.grey[500],
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Luxury hotel with free water park, near Chiang Mai Night Bazaar',
                         style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20)),
-                  ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    '1,000 verified Hotels.com guest reviews',
-                    style:
-                        TextStyle(fontWeight: FontWeight.normal, fontSize: 15),
-                  ),
-                  TextButton(
-                    onPressed: null,
-                    style: TextButton.styleFrom(
-                      primary: Colors.purpleAccent,
-                      onSurface: Colors.blue,
+                            fontWeight: FontWeight.normal, fontSize: 20),
+                      ),
                     ),
-                    child: const Text(
-                      'See all 1,000 reviews  >',
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 10, top: 50),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text("9.0/10 Superb",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20)),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      '1,000 verified Hotels.com guest reviews',
                       style: TextStyle(
                           fontWeight: FontWeight.normal, fontSize: 15),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(left: 10, top: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text("Popular amenities",
+                    TextButton(
+                      onPressed: null,
+                      style: TextButton.styleFrom(
+                        primary: Colors.purpleAccent,
+                        onSurface: Colors.blue,
+                      ),
+                      child: const Text(
+                        'See all 1,000 reviews  >',
                         style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20)),
-                  ),
-                  const SizedBox(height: 10),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const <Widget>[
-                          Icon(
-                            Icons.wifi,
-                            color: Colors.white,
-                          ),
-                          SizedBox(width: 10),
-                          Text("Free WiFi",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.normal, fontSize: 15)),
-                          SizedBox(width: 90),
-                          Icon(
-                            Icons.pool,
-                            color: Colors.white,
-                          ),
-                          SizedBox(width: 10),
-                          Text("Pool",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.normal, fontSize: 15)),
-                        ],
+                            fontWeight: FontWeight.normal, fontSize: 15),
                       ),
-                      const SizedBox(height: 10),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const <Widget>[
-                          Icon(
-                            Icons.ac_unit_sharp,
-                            color: Colors.white,
-                          ),
-                          SizedBox(width: 10),
-                          Text("Air Conditioning",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.normal, fontSize: 15)),
-                          SizedBox(width: 45),
-                          Icon(
-                            Icons.drive_eta,
-                            color: Colors.white,
-                          ),
-                          SizedBox(width: 10),
-                          Text("Free Parking",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.normal, fontSize: 15)),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const <Widget>[
-                          Icon(
-                            Icons.fitness_center,
-                            color: Colors.white,
-                          ),
-                          SizedBox(width: 10),
-                          Text("Gym",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.normal, fontSize: 15)),
-                          SizedBox(width: 120),
-                          Icon(
-                            Icons.thermostat,
-                            color: Colors.white,
-                          ),
-                          SizedBox(width: 10),
-                          Text("Refrigerator",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.normal, fontSize: 15)),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _loginUser,
-      //   tooltip: 'login',
-      //   child: const Icon(Icons.login),
-      // ),
-      bottomNavigationBar: Container(
-        height: 60,
-        color: Colors.blueAccent,
-        child: InkWell(
-          onTap: () => print('Select room'),
-          child: Padding(
-            padding: EdgeInsets.only(top: 20.0),
-            child: Column(
-              children: const <Widget>[
-                Text('Select a room', textAlign: TextAlign.center),
-              ],
-            ),
+              Container(
+                padding: const EdgeInsets.only(left: 10, top: 5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text("Popular amenities",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20)),
+                    ),
+                    const SizedBox(height: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const <Widget>[
+                            Icon(
+                              Icons.wifi,
+                              color: Colors.white,
+                            ),
+                            SizedBox(width: 10),
+                            Text("Free WiFi",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 15)),
+                            SizedBox(width: 90),
+                            Icon(
+                              Icons.pool,
+                              color: Colors.white,
+                            ),
+                            SizedBox(width: 10),
+                            Text("Pool",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 15)),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const <Widget>[
+                            Icon(
+                              Icons.ac_unit_sharp,
+                              color: Colors.white,
+                            ),
+                            SizedBox(width: 10),
+                            Text("Air Conditioning",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 15)),
+                            SizedBox(width: 45),
+                            Icon(
+                              Icons.drive_eta,
+                              color: Colors.white,
+                            ),
+                            SizedBox(width: 10),
+                            Text("Free Parking",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 15)),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const <Widget>[
+                            Icon(
+                              Icons.fitness_center,
+                              color: Colors.white,
+                            ),
+                            SizedBox(width: 10),
+                            Text("Gym",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 15)),
+                            SizedBox(width: 120),
+                            Icon(
+                              Icons.thermostat,
+                              color: Colors.white,
+                            ),
+                            SizedBox(width: 10),
+                            Text("Refrigerator",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 15)),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Image.asset(
+                'assets/images/map.jpg',
+                fit: BoxFit.cover,
+              )
+            ],
           ),
         ),
-      ),
-    );
+        floatingActionButton: InkWell(
+          onTap: () => {print('Select room')},
+          child: Container(
+            height: 50.0,
+            width: MediaQuery.of(context).size.width * 0.92,
+            decoration: BoxDecoration(
+                color: Color.fromRGBO(169, 207, 245, 1),
+                borderRadius: BorderRadius.circular(30)),
+            child: const Center(
+              child: Text(
+                'Select a room',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16.0,
+                    color: Colors.black),
+              ),
+            ),
+          ),
+        ));
   }
 }
