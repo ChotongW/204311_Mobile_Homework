24a03272f23b42123af83c116e7be473bf62bea7
unknown <wasin_was@cmu.ac.th>
2023-01-13 14:31:30 +0700

    try to add onTap on gridview

---
+++ hw6/lib/pokemon_list.dart
@@ -0,0 +1,33 @@
+import 'pokemon_api.dart';
+import 'pokemon_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class PokemonList extends StatelessWidget {
+  const PokemonList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PokemonService>(builder: (_, pokemonService, __) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Pokemon List'),
+        ),
+        body: GridView.builder(
+          itemCount: pokemonService.pokemonCount,
+          itemBuilder: (BuildContext context, int index) {
+            // return Image.network(genPokemonImageUrl(index + 1));
+            return GestureDetector(
+      onTap: () {
+        // callback function when the item is tapped
+        print('Item tapped: $index');
+      },
+      child: Image.network(genPokemonImageUrl(index + 1)),);
+          },
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3),
+        ),
+      );
+    });
+  }
+}
